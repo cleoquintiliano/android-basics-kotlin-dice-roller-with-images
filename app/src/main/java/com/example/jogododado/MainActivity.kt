aa2d2ae24ecba7419af0d8057d8166300a4d22f5
Cléo Quintiliano <cleoquintiliano@gmail.com>
2022-09-19 00:41:48 -0300

    Adicionar imagens ao app

--- app/src/main/java/com/example/jogododado/MainActivity.kt
+++ app/src/main/java/com/example/jogododado/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jogododado
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -16,8 +17,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoJogar: Button = findViewById(R.id.button)
-
         botaoJogar.setOnClickListener { jogarDado() }
+
+        //Faz um lançamento de dado quando o app é iniciado
+        jogarDado()
 
     }
 
@@ -29,9 +32,23 @@ class MainActivity : AppCompatActivity() {
         val dado = Dado(6)
         val jogaDado = dado.jogar()
 
-        //Atualize a tela com o lançamento de dados
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = jogaDado.toString()
+        //Atualize a tela com o lançamento de dados, Encontrando o ImageView no layout
+        val imagemDado: ImageView = findViewById(R.id.imageView)
+
+        //Determina qual ID de recurso drawable usar com base na jogada de dados
+        val drawableResource = when (jogaDado) {
+            1 -> R.drawable.dado_1
+            2 -> R.drawable.dado_2
+            3 -> R.drawable.dado_3
+            4 -> R.drawable.dado_4
+            5 -> R.drawable.dado_5
+            else -> R.drawable.dado_6
+        }
+        //Atualiza o ImageView com o ID de recurso drawable correto
+        imagemDado.setImageResource(drawableResource)
+
+        //Atualiza a descrição do conteúdo
+        imagemDado.contentDescription = jogaDado.toString()
     }
 }
 
